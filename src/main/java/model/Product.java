3dc00716c0dddf4b96e43553d28fe1e47ab3a66c
Seventhy-Seven <72259094+Seventhy-Seven@users.noreply.github.com>
2022-03-24 15:25:31 +0500

    Update Product.java

--- src/main/java/model/Product.java
+++ src/main/java/model/Product.java
@@ -1,2 +1,23 @@
-package model;public class Product {
+package model;
+import lombok.*;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+
+public class Product {
+    private int id;
+    private String title;
+    private int cost;
+    private List<Product> products;
+
+    public Product(int id, String title, int cost) {
+        this.id = id;
+        this.title = title;
+        this.cost = cost;
+    }
 }
